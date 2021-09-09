4fe9b9a5eee88fdea0f287dd912d24a15f75de71
mixelio <mixelios@gmail.com>
2021-09-09 14:02:26 +0300

    htmlComplete

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const langArrow = document.querySelector(".lang_arrow");
+
+if (langArrow) {
+  langArrow.addEventListener("click", function (e) {
+    const langSub = document.querySelector(".lang_sub");
+    langArrow.classList.toggle("_active");
+    langSub.classList.toggle("_open");
+  });
+}
+
+$(document).ready(function () {
+  $(".main_slider").slick({
+    arrows: true,
+    slidesToShow: 1,
+    infinite: true,
+    speed: 500,
+  });
+});
+
+$(document).ready(function () {
+  $(".benefits_slider").slick({
+    arrows: true,
+    slidesToShow: 3,
+    infinite: true,
+    speed: 500,
+  });
+});
+
+$(document).ready(function () {
+  $(".comment_slider").slick({
+    arrows: false,
+    slidesToShow: 3,
+    infinite: true,
+    speed: 500,
+    dots: true,
+    centerMode: true,
+  });
+});
