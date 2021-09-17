dd084b226bcbcbf2da26cfee067fe76e46a38ba6
mixelio <mixelios@gmail.com>
2021-09-17 12:17:37 +0300

    111

---
+++ js/blog.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const langArrow = document.querySelector(".lang_arrow");
+
+if (langArrow) {
+  langArrow.addEventListener("click", function (e) {
+    const langSub = document.querySelector(".lang_sub");
+    langArrow.classList.toggle("_active");
+    langSub.classList.toggle("_open");
+  });
+}
+
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".burger__menu");
+
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
